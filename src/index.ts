4594af69a032ccbdd4fa6539894c78f1d168ded3
Bert Vereecken <vereecken.bert@gmail.com>
2020-08-30 16:12:38 +0200

    chore: init of express and docker works.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Dit is een test hahaha');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_, res) => res.send('hello world'));
+
+app.listen(3000, () => console.log('listening on port 3000'));
