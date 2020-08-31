f90dfa61e8f56c7c760e5fe0c96c3df8d38978ab
Bert Vereecken <bert.vereecken@conundra.eu>
2020-08-31 21:22:09 +0200

    fix: fixed docker-compose connection with postgres

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
-import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './graphql/resolvers/hello';
@@ -14,22 +14,22 @@ const logger = winstonLogger('server');
 const main = async () => {
   logger.info('Initializing app...');
 
-  const connection = await createConnection({
-    type: 'postgres',
-    database: 'test',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    logging: true,
-    // TODO: enable symchronize only in dev
-    synchronize: true,
-    entities: [Todo],
-  })
-    .then((connection) => {
-      console.log('connection', connection);
-    })
-    .catch((err) => logger.error(err));
+  // const connection = await createConnection({
+  //   type: 'postgres',
+  //   database: 'test',
+  //   host: 'localhost',
+  //   port: 5432,
+  //   username: 'postgres',
+  //   password: 'postgres',
+  //   logging: true,
+  //   // TODO: enable symchronize only in dev
+  //   synchronize: true,
+  //   entities: [Todo],
+  // })
+  //   .then((connection) => {
+  //     console.log('connection', connection);
+  //   })
+  //   .catch((err) => logger.error(err));
 
   const app = express();
 
@@ -42,7 +42,9 @@ const main = async () => {
 
   apolloServer.applyMiddleware({ app });
 
-  app.listen(3000, () => logger.info(`Server listing on port ${process.env.PORT}`));
+  app.listen(process.env.PORT || 3000, () =>
+    logger.info(`Server listing on port ${process.env.PORT}`),
+  );
 };
 
 main().catch((err) => {
